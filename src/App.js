6357024c73e93b204f472a4f8c4f2d1e774bd56a
hyeseon-park <lightning350@naver.com>
2023-07-05 23:45:38 +0900

    swiper slide 개발

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Posts from "./components/Posts";
+import Slide from "./components/Slide";
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      <Slide />
     </div>
   );
 }
