b1a4266154b0b72d50c31e00e848543299867f69
hyeseon-park <lightning350@naver.com>
2023-07-24 23:09:19 +0900

    오류 수정

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,9 @@
 import { rest } from "msw";
-import page1 from "./page_1.json";
 
 export const handlers = [
   rest.get("/api/random", async (req, res, ctx) => {
     const page = req.url.searchParams.get("page");
     console.log(page);
-    return res(ctx.status(200), ctx.json(page1));
+    return res(ctx.status(200), ctx.json());
   }),
 ];
