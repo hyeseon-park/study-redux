7070779f18b5ce2cf618cd78c397cf2f35a25bae
hyeseon-park <lightning350@naver.com>
2023-07-24 23:07:10 +0900

    경로 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import GlobalStyles from "./shared/global/globalStyles";
 import MainPage from "./pages/MainPage";
 import AddressPage from "./pages/AddressPage";
 import SlidePage from "./pages/SlidePage";
 import NumberPage from "./pages/NumberPage";
+import PostPage from "./pages/PostPage";
+import Post3Page from "./pages/Post3Page";
+import CounterPage from "./pages/CounterPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Posts2 from "./components/Posts2";
+import Posts3 from "./components/Posts3";
+import Test from "./components/Test";
+import CommentContextProvider from "./store/CommentContext";
 
 const Layout = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <header>header</header>
+      <header>
+        header
+        <Link to="/posts3">페이지 이동</Link>
+      </header>
       <Outlet />
-      <footer>footer</footer>
+      <footer>
+        <Link to="/address">페이지 이동</Link>
+        <div onClick={() => navigate("/slide")}>페이지 이동</div>
+      </footer>
     </>
   );
 };
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="address" element={<AddressPage />} />
-            <Route path="slide" element={<SlidePage />} />
-            <Route path="number">
-              <Route path=":numberId" element={<NumberPage />} />
+      <QueryClientProvider client={queryClient}>
+        <CommentContextProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<MainPage />} />
+              <Route path="address" element={<AddressPage />} />
+              <Route path="slide" element={<SlidePage />} />
+              <Route path="posts" element={<PostPage />} />
+              <Route path="posts2" element={<Posts2 />} />
+
+              <Route path="posts3" element={<Post3Page />} />
+
+              <Route path="counter" element={<CounterPage />} />
+              <Route path="test" element={<Test />} />
+              <Route path="number">
+                <Route path=":numberId" element={<NumberPage />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </div>
+          </Routes>
+        </CommentContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
