506be7c52c1fb3a2642d5ab808ca5bcfbcbb00bb
hyeseon-park <lightning350@naver.com>
2023-07-24 23:07:21 +0900

    msw 추가

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import { worker } from "./mocks/browser";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+worker.start();
+
 root.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </RecoilRoot>
 );
